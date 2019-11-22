d63f11e9052d472480e555b3bbdf6ac1f6147cc5
qnedy <altmaisefequatro@gmail.com>
2019-11-22 17:24:59 -0300

    mongoose-model-addInDB-middleware

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const admin = require('./routes/admin');
 const path = require('path');
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 //Configurações
     //Body Parser
@@ -18,10 +18,21 @@ const path = require('path');
         app.set('view engine', 'handlebars');
 
     //Mongoose
-        //Em breve...
+        mongoose.Promise = global.Promise;
+        mongoose.connect('mongodb://localhost/blogapp').then(() => {
+            console.log("Conectado ao mongo.");
+        }).catch((err) => {
+            console.log("Erro ao se conectar ao mongo: " + err);
+        });
 
     //Public
         app.use(express.static(path.join(__dirname, "public"))); //dizendo pro express q public e a pasta de arquivos estáticos
+
+        //Middleware
+        app.use((req, res, next) => {
+            console.log("Chamou middleware!");
+            next();
+        });
 //Rotas
 app.use('/admin', admin);
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
 const router = express.Router();
+const mongoose = require('mongoose');
+require('../models/Categoria');
+const Categoria = mongoose.model('categorias');
 
 
 router.get('/', (req, res) => {
@@ -12,7 +14,25 @@ router.get('/posts', (req, res) => {
 });
 
 router.get('/categorias', (req, res) => {
-    res.send("Página de categorias");
+    res.render("admin/categorias");
+});
+
+router.get('/categorias/add', (req, res) => {
+    res.render("admin/addcategorias");
+});
+
+router.post('/categorias/nova', (req, res) => {
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then(() => {
+        console.log("Categoria cadastrada com sucesso!");
+    }).catch((err) => {
+        console.log("Erro ao cadastrar categoria: " + err);
+    });
+
 });
 
 
