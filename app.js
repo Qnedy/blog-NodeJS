f11dedf3ae29570d74d0e770ba8f54ff4df84f70
qnedy <altmaisefequatro@gmail.com>
2019-11-24 12:47:32 -0300

    sessions-formValidado-listCategorias

--- app.js
+++ app.js
@@ -7,8 +7,27 @@ const bodyParser = require('body-parser');
 const admin = require('./routes/admin');
 const path = require('path');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 //Configurações
+
+    //Session
+        app.use(session({
+            secret: "keyofsession",
+            resave: true,
+            saveUninitialized: true
+        }));
+
+        app.use(flash());
+
+    //Middlewarr
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash("success_msg");
+            res.locals.error_msg = req.flash("error_msg");
+            next();
+        });
+
     //Body Parser
         app.use(bodyParser.urlencoded({extended: true}));
         app.use(bodyParser.json());
--- routes/admin.js
+++ routes/admin.js
@@ -9,29 +9,52 @@ router.get('/', (req, res) => {
     res.render("admin/index");
 });
 
-router.get('/posts', (req, res) => {
-    res.send("Página de posts");
-});
-
 router.get('/categorias', (req, res) => {
-    res.render("admin/categorias");
+    Categoria.find().then((categorias) => {
+        res.render('admin/categorias', {categorias: categorias});
+    }).catch((err) => {
+        req.flash('error_msg', "Não foi possível carregar a lista de categorias.");
+        res.redirect('/admin');
+    });
 });
 
 router.get('/categorias/add', (req, res) => {
-    res.render("admin/addcategorias");
+    res.render('admin/addcategorias');
 });
 
 router.post('/categorias/nova', (req, res) => {
-    const novaCategoria = {
-        nome: req.body.nome,
-        slug: req.body.slug
+    
+
+    var erros = [];
+
+    if(!req.body.nome || typeof req.body.nome === undefined || req.body.nome === null){
+        erros.push({texto: "Nome inválido."});
     }
 
-    new Categoria(novaCategoria).save().then(() => {
-        console.log("Categoria cadastrada com sucesso!");
-    }).catch((err) => {
-        console.log("Erro ao cadastrar categoria: " + err);
-    });
+    if(!req.body.slug || typeof req.body.slug === undefined || req.body.slug === null){
+        erros.push({texto: "Slug inválido"});
+    }
+
+    if(req.body.nome.length < 2){
+        erros.push({texto: "Nome da categoria muito pequeno."});
+    }
+
+    if(erros.length > 0){
+        res.render('admin/addcategorias', {erros: erros});
+    }else{
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+
+        new Categoria(novaCategoria).save().then(() => {
+            req.flash('success_msg', "Categoria criada com sucesso!");
+            res.redirect('/admin/categorias');
+        }).catch((err) => {
+            req.flash('error_msg', "Categoria não pôde ser criada, tente novamente.");
+            res.redirect('/admin');
+        });
+    }
 
 });
 
